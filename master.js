859ff90f7006ce66f03b441fabcaf735ae1252aa
Memo Lopez <memolopez@me.com>
2014-02-18 14:59:35 -0600

    First working version
    Added button, needs an icon.

--- master.js
+++ master.js
@@ -1,3 +1,8 @@
 (function () {
-	
+	var url = (window.location.href).split("/")[4];
+
+	$('ul#actions li:nth-last-of-type(2)').after(
+		"<li><a href='http://www.goear.com/action/sound/get/" + url + 
+		"' id='addtofavorites'> Download"+
+		"</a></li>");
 })();
